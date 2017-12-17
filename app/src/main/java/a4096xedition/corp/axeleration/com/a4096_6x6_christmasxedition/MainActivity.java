cf8004c44d1981eb63d6b08e19395e3cc0763ec0
Zvukamar <fre3run@gmail.com>
2017-12-17 15:28:59 +0200

    Init commit

---
+++ app/src/main/java/a4096xedition/corp/axeleration/com/a4096_6x6_christmasxedition/MainActivity.java
@@ -0,0 +1,13 @@
+package a4096xedition.corp.axeleration.com.a4096_6x6_christmasxedition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
